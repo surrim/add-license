cea564053146af0ce70063a7b7985848093f73c2
surrim <root@surrim.org>
2025-12-25 22:31:36 +0100

    copyright info updated

--- add-license.sh
+++ add-license.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright 2015-2022 surrim
+# Copyright 2015-2025 surrim
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
